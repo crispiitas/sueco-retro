50229cecff886046a650623751cb7926263762b0
crispiitas <84731139+crispiitas@users.noreply.github.com>
2026-06-26 17:38:20 +0200

    Convertir en PWA para SFI

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'retrosvenska-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Courier+New:wght@400;700&display=swap'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activar y limpiar cachés antiguas
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Hacer que funcione sin conexión
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
